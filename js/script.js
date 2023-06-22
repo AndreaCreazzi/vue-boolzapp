6ff33b44283420d60bec2e9206708cf91d2f60cd
Andrea Creazzi <andreacreazzi@libero.it>
2023-06-22 15:38:52 +0200

    add removeMessage

--- js/script.js
+++ js/script.js
@@ -233,6 +233,9 @@ const app = createApp({
     setCurrentChat(targetIndex) {
       this.currentIndex = targetIndex;
     },
+    removeMessage(targetIndex) {
+      this.contacts[targetIndex]["messages"].splice(targetIndex, 1);
+    },
     addNewMessage(targetIndex) {
       if (this.newMessage !== null && this.newMessage !== "") {
         const message = {
